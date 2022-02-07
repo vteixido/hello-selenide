c7039ca9ea9c884df32fc5c3aca92f4befb4da36
Javier Rodriguez <profesor@sinensia.com>
2022-02-07 14:11:11 +0100

    Initial import

---
+++ src/test/java/com/sinensia/helloselenide/ImdbTest.java
@@ -0,0 +1,38 @@
+package com.sinensia.helloselenide;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ImdbTest {
+    MainPage mainPage = new MainPage();
+
+    @BeforeAll
+    public static void setUpAll() {
+        Configuration.browserSize = "1280x800";
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @BeforeEach
+    public void setUp() {
+        open("https://www.imdb.com/");
+    }
+
+    @Test
+    public void imdbSearch() {
+        $("#suggestion-search").sendKeys("el juego del calamar", Keys.ENTER);
+        $(By.linkText("Squid Game")).shouldBe(visible).click();
+    }
+}
