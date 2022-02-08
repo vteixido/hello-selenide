3397e8dd7f4482d0ec70175d5505cd36ab0aeb5e
Javier Rodriguez <profesor@sinensia.com>
2022-02-08 14:17:49 +0100

    Added page object and initial CartSuiteTest.

---
+++ src/test/java/com/sinensia/helloselenide/CartPage.java
@@ -0,0 +1,28 @@
+package com.sinensia.helloselenide;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.*;
+
+// http://localhost:3000/#!/
+public class CartPage {
+
+    private SelenideElement btnAddCola = $("html > body > robo-robobar > div > div:nth-of-type(2) > robo-place-order > div:nth-of-type(2) > table > tbody > tr:nth-of-type(1) > td:nth-of-type(3) > div > div > div:nth-of-type(2) > button");
+
+    private SelenideElement btnAddBeer = $("html > body > robo-robobar > div > div:nth-of-type(2) > robo-place-order > div:nth-of-type(2) > table > tbody > tr:nth-of-type(2) > td:nth-of-type(3) > div > div > div:nth-of-type(2) > button");
+
+    private SelenideElement btnAddWine = $("html > body > robo-robobar > div > div:nth-of-type(2) > robo-place-order > div:nth-of-type(2) > table > tbody > tr:nth-of-type(3) > td:nth-of-type(3) > div > div > div:nth-of-type(2) > button");
+
+    private SelenideElement txtTotal = $("th[class='ng-binding']");
+
+    private SelenideElement btnCheckout = $(".btn-success");
+
+    public void addCola() {
+        btnAddCola.click();
+    }
+
+    public SelenideElement total() {
+        return txtTotal;
+    }
+
+}
---
+++ src/test/java/com/sinensia/helloselenide/CartSuiteTest.java
@@ -0,0 +1,36 @@
+package com.sinensia.helloselenide;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CartSuiteTest {
+    CartPage cartPage =  new CartPage();
+
+    @BeforeAll
+    public static void setUpAll() {
+        Configuration.browserSize = "1280x800";
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @BeforeEach
+    public void setUp() {
+        open("http://localhost:3000/");
+    }
+
+    @Test
+    public void colaTest() {
+        cartPage.addCola();
+        cartPage.total().shouldBe(text("€1.25"));
+    }
+}
